c14dfb3b206df68229bbef1841ccc156a9bb247e
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2025-11-21 11:58:55 -0300

    fix(auditoria): remove chamada de token inútil

--- src/middlewares/requireAuth.ts
+++ src/middlewares/requireAuth.ts
@@ -4,22 +4,19 @@ export async function requireAuth(
   req: FastifyRequest,
   reply: FastifyReply
 ) {
-  try {
-    // Verifica se existe o token
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      return reply.status(401).send({ message: "Token não enviado" });
-    }
-
-    const token = authHeader.replace("Bearer ", "");
-
-    // Verifica a assinatura usando o plugin do Fastify-JWT
-    const decoded = await req.jwtVerify();
-
-    // Salva no request para outros handlers/middlewares
-    (req as any).user = decoded;
-
-  } catch (error) {
-    return reply.status(401).send({ message: "Token inválido" });
+  // Verifica se existe o token
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return reply.status(401).send({ message: "Token não enviado" });
   }
+
+  if (!authHeader.startsWith("Bearer ")) {
+    return reply.status(401).send({ message: "Formato de token inválido" });
+  }
+
+  // Verifica a assinatura usando o plugin do Fastify-JWT
+  const decoded = await req.jwtVerify();
+
+  // Salva no request para outros handlers/middlewares
+  (req as any).user = decoded;
 }
